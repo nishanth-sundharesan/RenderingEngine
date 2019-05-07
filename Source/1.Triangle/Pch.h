26bead6d7aa7a141b032eae7add2e228eb1755c0
nishanth-sundharesan <nishanth.reddy992@gmail.com>
2019-05-07 16:58:41 -0400

    Changed the project settings to use Precompiled headers. Updated the project settings for all configurations. (Previously, it was only for Debug | x86 config)

---
+++ Source/1.Triangle/Pch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Windows libraries
+#include <windows.h>
+
+// DirectX library
+#include <d3d11.h>
+
+// DirectX SIMD math library
+#include <DirectXMath.h>
+
+// Std libraries
+#include <vector>
